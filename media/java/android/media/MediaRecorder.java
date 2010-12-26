fa25ba45d060663b33d4bd4dddcf4101fe871703
xinyu <ac.milan@miui.com>
2010-12-26 16:46:57 -0500

    media: support FM Radio audio routing in audio flinger
    Change-Id: I942a12217e714962bcfe3c93f72ca28be47e808b

--- media/java/android/media/MediaRecorder.java
+++ media/java/android/media/MediaRecorder.java
@@ -154,6 +154,8 @@ public class MediaRecorder
          * is available.
          */
         public static final int VOICE_COMMUNICATION = 7;
+        public static final int FM_RX = 8;
+        public static final int FM_RX_A2DP = 9;
     }
 
     /**

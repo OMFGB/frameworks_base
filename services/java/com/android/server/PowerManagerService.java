be8ca7d7dc5811cfe9fcc091046d872db196ba60
Pedlar <pedlar88@gmail.com>
2011-01-01 18:24:11 -0500

    Galaxy S Style Power Widget
    Change-Id: I27970609f71a2e53e39d276102900d51b763f785
    
    Make it so the widget shows as on by default.
    
    Only modify the view of the bar, if we want to.
    
    Check to make sure the Uri we are modding is the one we want to.
    This is to prevent the bar from be disabled for some paradoxial reason.
    
    Added Flashlight button for devices with an LED. And a Sleep button
    
    Don't instantiate unused power buttons in StatusBarService.
    
    New power buttons in the StatusBar is a great thing. But, on my HTC Hero,
    I got a lot of the following error in logcat:
    
    E/FlashlightButton(  133): getFlashlightEnabled failed
    E/FlashlightButton(  133): java.io.FileNotFoundException: /sys/class/leds/flashlight/brightness (No such
    E/FlashlightButton(  133):      at org.apache.harmony.luni.platform.OSFileSystem.openImpl(Native Method)
    E/FlashlightButton(  133):      at org.apache.harmony.luni.platform.OSFileSystem.open(OSFileSystem.java:
    E/FlashlightButton(  133):      at java.io.FileInputStream.<init>(FileInputStream.java:82)
    E/FlashlightButton(  133):      at java.io.FileInputStream.<init>(FileInputStream.java:134)
    E/FlashlightButton(  133):      at com.android.server.status.widget.FlashlightButton.getFlashlightEnable
    E/FlashlightButton(  133):      at com.android.server.status.widget.FlashlightButton.updateState(Flashli
    E/FlashlightButton(  133):      at com.android.server.status.StatusBarService.updateStates(StatusBarServ
    E/FlashlightButton(  133):      at com.android.server.status.StatusBarService.updateWidget(StatusBarServ
    E/FlashlightButton(  133):      at com.android.server.status.StatusBarService.access$800(StatusBarServic
    E/FlashlightButton(  133):      at com.android.server.status.StatusBarService$7.onReceive(StatusBarServi
    E/FlashlightButton(  133):      at android.app.ActivityThread$PackageInfo$ReceiverDispatcher$Args.run(Ac
    E/FlashlightButton(  133):      at android.os.Handler.handleCallback(Handler.java:587)
    E/FlashlightButton(  133):      at android.os.Handler.dispatchMessage(Handler.java:92)
    E/FlashlightButton(  133):      at android.os.Looper.loop(Looper.java:123)
    E/FlashlightButton(  133):      at com.android.server.ServerThread.run(SystemServer.java:524)
    
    This is beacause, all power buttons are instantiate, even if unused or
    functionnality is missing. So, I remove most calls to getInstance() for
    all of them and use a HashMap to store the buttons used and only call
    them in updateStates() and updateWidget().d
    
    Additionnaly, my change reduce memory usage of the status bar.
    
    Fixed tabs, some indents and long lines.
    
    Change-Id: I22b98bddc791c547d02b50a09b561aa469cc25b7
    
    Fix 2g/3g toggle on UMTS networks
    (the same fix has been already applied to Settings app power widget)
    
    Change-Id: I255359ba4a6ba3d384140c4c9bd5f73e780b9037
    
    Removed Catch error, as it is uneccesary, and causes log spam on devices with no flash
    
    Cleanup notification bar flashlight widget to use Torch app,
    and add state storage for flashlight.
    
    Change-Id: I6d4ad46318a7593809e7ff63601f8bd70db64a0c
    
    Add setting to hide notification pulldown after clicking a power widget button
    
    Change-Id: Ibbb2da79ef93502990d122d1f31664e578a44a79
    
    Restore notification bar flashlight toggle using correct brightness
    setting.
    
    Change-Id: I5a35a4eb399d54613b782eac78ca1d1dad7cecf5
    
    allow the light sensor to be ignored when flashlight is on
    
    on certain devices, the flashlight affects the light sensor, causing the display
    autobrightness to switch to the maximum level when flashlight is on
    
    Change-Id: I22d623ce5457fc3ae177b609da8b51904bc73e83
    
    Fix NPE when checking TORCH_STATE on first boot.
    
    Change-Id: I24fa8e62c9a2bbac693ff9825988e04384dd6d75

--- services/java/com/android/server/PowerManagerService.java
+++ services/java/com/android/server/PowerManagerService.java
@@ -67,6 +67,7 @@ import static android.provider.Settings.System.SCREEN_OFF_TIMEOUT;
 import static android.provider.Settings.System.STAY_ON_WHILE_PLUGGED_IN;
 import static android.provider.Settings.System.WINDOW_ANIMATION_SCALE;
 import static android.provider.Settings.System.TRANSITION_ANIMATION_SCALE;
+import static android.provider.Settings.System.TORCH_STATE;
 
 import java.io.FileDescriptor;
 import java.io.IOException;
@@ -246,6 +247,8 @@ class PowerManagerService extends IPowerManager.Stub
     private int mWarningSpewThrottleCount;
     private long mWarningSpewThrottleTime;
     private int mAnimationSetting = ANIM_SETTING_OFF;
+    private boolean mFlashlightAffectsLightSensor;
+    private boolean mIgnoreLightSensor;
 
     // Must match with the ISurfaceComposer constants in C++.
     private static final int ANIM_SETTING_ON = 0x01;
@@ -452,6 +455,12 @@ class PowerManagerService extends IPowerManager.Stub
                 // DIM_SCREEN
                 //mDimScreen = getInt(DIM_SCREEN) != 0;
 
+                int torchState = Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.TORCH_STATE, 0);
+                mIgnoreLightSensor = (torchState > 0) && mFlashlightAffectsLightSensor;
+
+                updateLightSettings();
+
                 // SCREEN_BRIGHTNESS_MODE, default to manual
                 setScreenBrightnessMode(getInt(SCREEN_BRIGHTNESS_MODE,
                         Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL));
@@ -609,9 +618,12 @@ class PowerManagerService extends IPowerManager.Stub
                         + Settings.System.NAME + "=?) or ("
                         + Settings.System.NAME + "=?) or ("
                         + Settings.System.NAME + "=?) or ("
+                        + Settings.System.NAME + "=?) or ("
+                        + Settings.System.NAME + "=?) or ("
                         + Settings.System.NAME + "=?)",
                 new String[]{STAY_ON_WHILE_PLUGGED_IN, SCREEN_OFF_TIMEOUT, DIM_SCREEN,
-                        SCREEN_BRIGHTNESS_MODE, WINDOW_ANIMATION_SCALE, TRANSITION_ANIMATION_SCALE},
+                        SCREEN_BRIGHTNESS_MODE, WINDOW_ANIMATION_SCALE, TRANSITION_ANIMATION_SCALE,
+                        TORCH_STATE, Settings.System.LIGHTS_CHANGED},
                 null);
         mSettings = new ContentQueryMap(settingsCursor, Settings.System.NAME, true, mHandler);
         SettingsObserver settingsObserver = new SettingsObserver();
@@ -2971,7 +2983,8 @@ class PowerManagerService extends IPowerManager.Stub
         public void onSensorChanged(SensorEvent event) {
             synchronized (mLocks) {
                 // ignore light sensor while screen is turning off
-                if (isScreenTurningOffLocked()) {
+                // or when flashlight would affect it
+                if (isScreenTurningOffLocked() || mIgnoreLightSensor) {
                     return;
                 }
 

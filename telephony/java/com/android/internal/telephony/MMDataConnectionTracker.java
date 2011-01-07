9a14df786a13178997ce09fa9b47967890b30436
Ajay Nambi <anambi@codeaurora.org>
2011-01-07 11:10:18 -0800

    Data Activity State fix: Include DORMANT state
    Change-Id: I97a3f52a13b33b66f955769e0c5c64cb1ef5edd2
    CRs-Fixed: 268894

--- telephony/java/com/android/internal/telephony/MMDataConnectionTracker.java
+++ telephony/java/com/android/internal/telephony/MMDataConnectionTracker.java
@@ -1472,6 +1472,9 @@ public class MMDataConnectionTracker extends DataConnectionTracker {
                 case DATAINANDOUT:
                     ret = DataActivityState.DATAINANDOUT;
                     break;
+                case DORMANT:
+                    ret = DataActivityState.DORMANT;
+                    break;
             }
         }
         return ret;

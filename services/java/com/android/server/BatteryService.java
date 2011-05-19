43e5e0b8ff8874d2a90037250ebbc45fbf501325
r2DoesInc <r2doesinc@poiteeserver>
2011-05-18 23:40:35 -0400

    Battery options.

--- services/java/com/android/server/BatteryService.java
+++ services/java/com/android/server/BatteryService.java
@@ -44,6 +44,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import static android.provider.Settings.System.BATTERY_OPTION;
 
 /**
  * <p>BatteryService monitors the charging status, and charge level of the device
@@ -120,6 +121,8 @@ class BatteryService extends Binder {
 
     private boolean mSentLowBatteryBroadcast = false;
 
+    private int mBatteryOption = 1;
+    
     public BatteryService(Context context) {
         mContext = context;
         mBatteryStats = BatteryStatsService.getService();
@@ -130,6 +133,8 @@ class BatteryService extends Binder {
                 com.android.internal.R.integer.config_lowBatteryCloseWarningLevel);
 
         mUEventObserver.startObserving("SUBSYSTEM=power_supply");
+
+	mBatteryOption = Settings.System.getInt(mContext.getContentResolver(), BATTERY_OPTION, 1);
 
         // set initial status
         update();
@@ -437,13 +442,21 @@ class BatteryService extends Binder {
         }
     }
 
-    private final int getIcon(int level) {
+    public final int getIcon(int level) {
         if (mBatteryStatus == BatteryManager.BATTERY_STATUS_CHARGING) {
             return com.android.internal.R.drawable.stat_sys_battery_charge;
         } else if (mBatteryStatus == BatteryManager.BATTERY_STATUS_DISCHARGING ||
                 mBatteryStatus == BatteryManager.BATTERY_STATUS_NOT_CHARGING ||
                 mBatteryStatus == BatteryManager.BATTERY_STATUS_FULL) {
+
+	  if (mBatteryOption == 0) {
+            return com.android.internal.R.drawable.stat_sys_battery_percentage;
+          } else if (mBatteryOption == 1 ) {
             return com.android.internal.R.drawable.stat_sys_battery;
+          } else {
+            return com.android.internal.R.drawable.stat_sys_battery;
+          }
+
         } else {
             return com.android.internal.R.drawable.stat_sys_battery_unknown;
         }

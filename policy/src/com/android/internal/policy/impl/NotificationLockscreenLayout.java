0812cf3882c0036eeb93ea610b0a4147f867f592
r2DoesInc <r2doesinc@gmail.com>
2011-05-30 18:54:45 -0400

    Add SMS button to lockscreen. Remove CM mini battery and clock mods.

---
+++ policy/src/com/android/internal/policy/impl/NotificationLockscreenLayout.java
@@ -0,0 +1,30 @@
+package com.android.internal.policy.impl;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class NotificationLockscreenLayout extends ListView{
+	
+	private int mNotifiers = 0;
+	private final int MAXNOTIFIERS = 4;
+	
+	
+	 public NotificationLockscreenLayout(Context context) {
+	        this(context, null);
+	        
+	   
+	    }
+
+	    public NotificationLockscreenLayout(Context context, AttributeSet attrs) {
+	        super(context, attrs);
+	        
+
+	        
+	    }
+	
+	
+	
+	
+
+}

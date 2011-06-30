e674d8db3935b4336749ea6243a81ae067324fda
r2DoesInc <r2doesinc@gmail.com>
2011-06-30 04:06:13 -0400

    Add temp tie in for God Mode setting.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1957,6 +1957,12 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_ALWAYS_BATTERY = "lockscreen_always_battery";
  
+	/**
+	 * Whether or not to allow orientation changes in the lockscreen.
+	 */
+
+        public static final String LOCKSCREEN_ORIENTATION = "lockscreen_orientation";
+
         /**
 	 * Whether lockscreen shortcuts should be shown on lockscreen
 	 * @hide

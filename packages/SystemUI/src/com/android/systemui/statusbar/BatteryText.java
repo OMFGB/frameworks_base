472c6cd40a49409623f95b272810462e3eda3392
sbrissen <sbrissen@Hotmail.com>
2011-07-22 16:05:58 -0400

    comments

--- packages/SystemUI/src/com/android/systemui/statusbar/BatteryText.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BatteryText.java
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Credit: birgertime for auto color idea
+ *        -sbrissen
  */
 
 package com.android.systemui.statusbar;

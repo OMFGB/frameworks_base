880a7315239158577385802170e65c28d8fd2468
r2DoesInc <r2doesinc@gmail.com>
2011-08-01 09:36:59 -0400

    redundant code.

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -140,8 +140,6 @@ public class NotificationManagerService extends INotificationManager.Stub
     private boolean mBatteryFull;
     private NotificationRecord mLedNotification;
     
-    private boolean mHideAdb;
-
     private static final int BATTERY_LOW_ARGB = 0xFFFF0000; // Charging Low - red solid on
     private static final int BATTERY_MEDIUM_ARGB = 0xFFFFFF00;    // Charging - orange solid on
     private static final int BATTERY_FULL_ARGB = 0xFF00FF00; // Charging Full - green solid on
@@ -1273,7 +1271,6 @@ public class NotificationManagerService extends INotificationManager.Stub
     // security feature that we don't want people customizing the platform
     // to accidentally lose.
     private void updateAdbNotification(boolean adbEnabled) {
-	ContentResolver resolver = mContext.getContentResolver();
         if (adbEnabled) {
             if ("0".equals(SystemProperties.get("persist.adb.notify")) ||
                             Settings.Secure.getInt(mContext.getContentResolver(),
@@ -1312,16 +1309,11 @@ public class NotificationManagerService extends INotificationManager.Stub
                             "com.android.settings.DevelopmentSettings"));
                     PendingIntent pi = PendingIntent.getActivity(mContext, 0,
                             intent, 0);
-		    mHideAdb = (Settings.System.getInt(resolver, Settings.System.HIDE_ADB_ICON, 0) == 1);
-
-		    if(!mHideAdb){
-                       mAdbNotification.setLatestEventInfo(mContext, title, message, pi);
-
+		      mAdbNotification.setLatestEventInfo(mContext, title, message, pi);
 		      mAdbNotificationShown = true;
 		      notificationManager.notify(
 			      com.android.internal.R.string.adb_active_notification_title,
 			      mAdbNotification);
-		    }
                 }
             }
 

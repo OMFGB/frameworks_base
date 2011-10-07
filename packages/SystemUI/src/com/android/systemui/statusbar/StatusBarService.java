e315c5694805b22e1396e0153e776cc4bd78f1f1
r2doesinc <r2doesinc@gmail.com>
2011-10-07 02:41:44 -0400

    This allows the ability to control brightness by sliding across the statusbar.
    The original work was done daryelv on his github : https://github.com/daryelv/android_frameworks_base
    
    However author didn't have time to submit / make it configurable through cm parts. So i took up the project,
    
    rebased it on 2.3.4 and allowed it to be configurable via cmparts.
    
    Option can be found under Cm Parts -> Interface -> Status Bar Tweaks
    
    Change-Id: I202e3641bcea5dd90daaab2cf804d59b40724df4
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/StatusBarService.java

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarService.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarService.java
@@ -25,6 +25,8 @@ import com.android.systemui.statusbar.StatusBarView.SettingsObserver;
 import com.android.systemui.statusbar.powerwidget.PowerWidget;
 import com.android.systemui.statusbar.powerwidget.PowerWidgetBottom;
 
+import android.os.IPowerManager;
+import android.provider.Settings.SettingNotFoundException;
 import android.app.ActivityManagerNative;
 import android.app.Dialog;
 import android.app.Notification;
@@ -205,6 +207,9 @@ public class StatusBarService extends Service implements CommandQueue.Callbacks 
     
     // tracking for the last visible power widget id so hide toggle works properly
     int mLastPowerToggle = 1;
+
+    // for brightness control on status bar
+    int mLinger = 0;
 
     private class ExpandedDialog extends Dialog {
         ExpandedDialog(Context context) {
@@ -1106,7 +1111,7 @@ public class StatusBarService extends Service implements CommandQueue.Callbacks 
         final int hitSize = statusBarSize*2;
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             final int y = (int)event.getRawY();
-
+            mLinger = 0;
             if (!mExpanded) {
                 mViewDelta = statusBarSize - y;
             } else {
@@ -1130,12 +1135,65 @@ public class StatusBarService extends Service implements CommandQueue.Callbacks 
             mVelocityTracker.addMovement(event);
             final int minY = statusBarSize + mCloseView.getHeight();
             if (event.getAction() == MotionEvent.ACTION_MOVE) {
-                int y = (int)event.getRawY();
+		int y = (int)event.getRawY();
                 if (mAnimatingReveal && y < minY) {
-                    // nothing
+                        try {
+                                        //Credit for code goes to daryelv github : https://github.com/daryelv/android_frameworks_base
+                                        // See if finger is moving left/right an adequate amount
+                                        mVelocityTracker.computeCurrentVelocity(1000);
+                                        float yVel = mVelocityTracker.getYVelocity();
+                                        if (yVel < 0) {
+                                                yVel = -yVel;
+                                        }
+                                        if (yVel < 50.0f) {
+                                                if (mLinger > 50) {
+                                                        // Check that Auto-Brightness not enabled
+                                                        Context context = mStatusBarView.getContext();
+                                                        boolean auto_brightness = false;
+                                                        int brightness_mode = 0;
+                                                        try {
+                                                                brightness_mode = Settings.System.getInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE);
+                                                        }catch (SettingNotFoundException e){
+                                                                auto_brightness = false;
+                                                        }
+                                                        auto_brightness = (brightness_mode == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC);
+                                                        if (auto_brightness)
+                                                        {
+                                                                // do nothing - Don't manually set brightness from statusbar
+                                                        }
+                                                        else
+                                                        {
+                                                                // set brightness according to x position on statusbar
+                                                                float x = (float)event.getRawX();
+                                                                float screen_width = (float)(context.getResources().getDisplayMetrics().widthPixels);
+                                                                // Brightness set from the 90% of pixels in the middle of screen, can't always get to the edges
+                                                                int new_brightness = (int)(((x - (screen_width * 0.05f))/(screen_width * 0.9f)) * (float)android.os.Power.BRIGHTNESS_ON );
+                                                                // don't let screen go completely dim or past 100% bright
+                                                                if (new_brightness < 10) new_brightness = 10;
+                                                                if (new_brightness > android.os.Power.BRIGHTNESS_ON ) new_brightness = android.os.Power.BRIGHTNESS_ON;
+                                                                // Set the brightness
+                                                                try {
+                                                                        IPowerManager.Stub.asInterface(ServiceManager.getService("power")).setBacklightBrightness(new_brightness);
+                                                                        Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, new_brightness);
+                                                                }catch (Exception e){
+                                                                        Slog.w(TAG, "Setting Brightness failed: " + e);
+                                                                }
+                                                       }
+                                                }
+                                                else
+                                                {
+                                                        mLinger++;
+                                                }
+                                        }
+                                        else
+                                        {
+                                                mLinger = 0;
+                                        }
+                }catch (Exception e){
+                }
                 } else  {
                     mAnimatingReveal = false;
-                    updateExpandedViewPos(y + mViewDelta);
+                    updateExpandedViewPos(y + (mViewDelta));
                 }
             } else if (event.getAction() == MotionEvent.ACTION_UP) {
                 mVelocityTracker.computeCurrentVelocity(1000);

de109095b3bcfcfcfa8a8e0a05f61c584e1942ad
r2doesinc <r2doesinc@gmail.com>
2011-10-20 18:06:40 -0400

    Add screenshot share from cm

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1969,6 +1969,13 @@ public final class Settings {
 	   */
 	   public static final String LOCKSCREEN_ALWAYS_BATTERY = "lockscreen_always_battery";
 	 
+        /**
+         * Specifies whether to show share dialog after
+         * taking screenshot
+         * @hide
+         */
+        public static final String SHARE_SCREENSHOT = "share_screenshot";
+
 
 
         /**
